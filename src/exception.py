029e558eabd3f022e6c8b4ffb46f0f06274e789a
Mayu21ad <mayuri.21ad@kct.ac.in>
2024-10-05 09:21:40 +0530

    eda performed

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def err_msg_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -12,7 +13,7 @@ def err_msg_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = err_msg_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
